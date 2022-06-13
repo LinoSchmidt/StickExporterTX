179d7f9d7b12f370fb2f9aab25c72479b8400bc1
Lino Schmidt <linoschmidt@lino3d.de>
2022-06-13 19:45:10 +0200

    Fixed default output path not found

--- src/components/paths.ts
+++ src/components/paths.ts
@@ -1,11 +1,10 @@
 import path from 'path';
 import {app} from '@electron/remote';
-import getPath from 'platform-folders';
 
 export const dataPath = app.getPath('userData');
 export const SettingPath = path.join(dataPath, "settings.xml");
 
-export const defaultOutputPath = path.join(getPath('videos') || "", "StickExporterTX");
+export const defaultOutputPath = path.join(app.getPath('videos'), "StickExporterTX");
 
 export const blenderPath = path.join("assets", "blender", "blender");
 export const templatePath = path.join("assets", "template.blend");
