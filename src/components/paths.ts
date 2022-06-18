7fc8c2efa2d2c2efa741df68259efc5188df425e
Lino Schmidt <linoschmidt@lino3d.de>
2022-06-18 23:23:05 +0200

    Added multiplatform paths support

--- src/components/paths.ts
+++ src/components/paths.ts
@@ -2,19 +2,20 @@ import path from 'path';
 import {app} from '@electron/remote';
 
 export const dataPath = app.getPath('userData');
+export const appPath = app.getAppPath().replace("app.asar", "");
 export const SettingPath = path.join(dataPath, "settings.xml");
 
 export const defaultOutputPath = path.join(app.getPath('videos'), "StickExporterTX");
 
 let platformFolder = "";
-if(process.platform === "win32"){
+if(process.platform === "win32") {
     platformFolder = "windows";
-} else if(process.platform === "darwin"){
+} else if(process.platform === "darwin") {
     platformFolder = "darwin";
-} else if(process.platform === "linux"){
+} else if(process.platform === "linux") {
     platformFolder = "linux";
 }
 
-export const blenderPath = path.join("dependencies", platformFolder, "blender", "blender");
-export const templatePath = path.join("dependencies", "template.blend");
-export const blenderScriptPath = path.join("dependencies", "blenderScript.py");
+export const blenderPath = path.join(appPath, "dependencies", platformFolder, "blender", "blender");
+export const templatePath = path.join(appPath, "dependencies", "template.blend");
+export const blenderScriptPath = path.join(appPath, "dependencies", "blenderScript.py");
